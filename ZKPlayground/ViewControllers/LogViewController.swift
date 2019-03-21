26f63b21f92b96aa360f7abf33a4098c0a736ee2
Ronald Mannak <ronaldmannak@me.com>
2019-03-20 23:24:04 -0700

    Basic viewcontrollers

---
+++ ZKPlayground/ViewControllers/LogViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LogViewController.swift
+//  ZKPlayground
+//
+//  Created by Ronald "Danger" Mannak on 3/20/19.
+//  Copyright © 2019 A Puzzle A Day. All rights reserved.
+//
+
+import Cocoa
+
+class LogViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
