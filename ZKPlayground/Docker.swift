fffe4c74daa1fb23601ce860c5307b903ebc7dd3
Ronald Mannak <ronaldmannak@me.com>
2019-03-21 17:36:52 -0700

    Docker permission denied

---
+++ ZKPlayground/Docker.swift
@@ -0,0 +1,140 @@
+//
+//  Docker.swift
+//  ZKPlayground
+//
+//  Created by Ronald "Danger" Mannak on 3/21/19.
+//  Copyright © 2019 A Puzzle A Day. All rights reserved.
+//
+
+import Foundation
+
+protocol DockerProtocol: class {
+    
+    // Docker received data from the docker image
+    func docker(_ docker: Docker, didReceiveStdout: String)
+    
+    // Docker received an error from the docker image
+    func docker(_ docker: Docker, didReceiveStderr: String)
+    
+    // Docker sent stdin to the docker image from the app
+    func docker(_ docker: Docker, didReceiveStdin: String)
+}
+
+class Docker: Operation {
+    
+    weak var delegate: DockerProtocol?
+    
+    /// If any of the commands in the script returns non-zero,
+    /// the script will cancel and forward the exit code
+    /// 0 is success, anything else is an error
+    /// http://www.tldp.org/LDP/abs/html/exitcodes.html
+    private (set) var exitStatus: Int?
+    
+    /// Complete output
+//    private (set) var output: String = ""
+
+    
+    private let task = Process()
+    
+    private let stdoutPipe = Pipe()
+    private let stderrPipe = Pipe()
+    private let stdinPipe = Pipe()
+    private var notifications = [NSObjectProtocol]()
+//    private var stdoutNotification: NSObjectProtocol!
+//    private var stderrNotification: NSObjectProtocol!
+//    private var stdinNotification: NSObjectProtocol!
+    
+    /// Path of the directory the .code file is in, e.g. "/MySource/"
+    private let workDirectoryPath: String
+    
+    /// Name of the .code file, e.g. "HelloWorld.code"
+    private var filename: String
+    
+    /// the name of the directory mapped to workDirectoryPath
+    private static let dockerDirectoryPath = "playground"
+    
+    // TODO: Add arguments?
+    
+    init(workDirectory: String, filename: String) {
+        self.workDirectoryPath = workDirectory
+        self.filename = filename
+        
+        // Set up task
+    }
+    
+    
+    /// Runs (and if needed, installs) Zokrates in a Docker image
+    /// Format is: docker run -v /Users/davidhasselhoff/Code/:/home/zokrates/playground -ti zokrates/zokrates /bin/bash
+    override func main() {
+        
+        defer { _ = notifications.map { NotificationCenter.default.removeObserver($0) } }
+        
+        guard isCancelled == false else { return }
+        
+        // Set up task
+        self.task.environment = ProcessInfo().environment
+        self.task.environment?.updateValue("/usr/local/bin/:/usr/bin:/bin:/usr/sbin:/sbin", forKey: "PATH")
+        task.launchPath = "/usr/local/bin/docker" // TODO: use which path
+        task.arguments = ["run", "-v /Users/ronalddanger/Development/Temp/zk/:/home/zokrates/zk", "-ti", "zokrates/zokrates", "/bin/bash"]
+        
+        // Set exitStatus at exit
+        self.task.terminationHandler = { task in
+            self.exitStatus = Int(task.terminationStatus)
+        }
+        /*
+        // Handle I/O
+        self.task.standardOutput = self.stdoutPipe
+        self.task.standardError = self.stderrPipe
+        self.task.standardInput = self.stdinPipe
+
+        self.capture(self.stdoutPipe) { stdout in
+            self.delegate?.docker(self, didReceiveStdout: stdout)
+        }
+        self.capture(self.stderrPipe) { stderr in
+            self.delegate?.docker(self, didReceiveStderr: stderr)
+            
+        } */
+//        self.capture(self.stdinPipe)
+        
+        task.launch()
+        task.waitUntilExit()
+    }
+    
+    private func capture(_ pipe: Pipe, dataReceived: @escaping (String) -> Void) {
+        
+        let notification = NotificationCenter.default.addObserver(forName: .NSFileHandleDataAvailable, object: pipe.fileHandleForReading , queue: nil) { notification in
+            
+            let output = pipe.fileHandleForReading.availableData
+            
+            guard let outputString = String(data: output, encoding: String.Encoding.utf8), !outputString.isEmpty else { return }
+            
+            print(outputString)
+            dataReceived(outputString)
+            
+            pipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+        }
+        
+        notifications.append(notification)
+            
+        pipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+    }
+    
+    
+    /// Compiles code, returns warnings and errors
+    func compile() {
+        
+    }
+    
+    /// Compiles and builds product and proofs
+    func build() {
+        
+    }
+    
+    /// Exits Docker
+    override func cancel() {
+        
+        // Send 'exit' command to docker
+//        if task.isRunning { task.interrupt() }
+        super.cancel()
+    }
+}
--- ZKPlayground/Editor Window/EditorWindowController.swift
+++ ZKPlayground/Editor Window/EditorWindowController.swift
@@ -12,9 +12,28 @@ class EditorWindowController: NSWindowController {
     
     override var document: AnyObject? {
         didSet {
+            
             contentViewController?.representedObject = document
+            
+            guard let document = document as? Document, document.fileURL != self.fileURL else { return }
+            
+            self.fileURL = document.fileURL
+            dockerQueue.cancelAllOperations()
+            
+            guard self.fileURL != nil else { return }
+            
+            // (Re)launch Docker
+            self.runDocker(nil)
+            
+            // TODO: KVO to check if file location has changed?
         }
     }
+    
+    private var fileURL: URL?
+    private var filename: String? { return self.fileURL?.lastPathComponent }
+    private var workDirectory: String? { return self.fileURL?.deletingLastPathComponent().path }
+    
+    let dockerQueue = OperationQueue()
 
     override func windowDidLoad() {
         super.windowDidLoad()
@@ -22,4 +41,43 @@ class EditorWindowController: NSWindowController {
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
 
+}
+
+// Docker extensions
+extension EditorWindowController {
+    
+    @IBAction func runDocker(_ sender: Any?) {
+        
+        print("Run docker called")
+        guard let filename = self.filename, let workDirectory = self.workDirectory else { return }
+        
+        let docker = Docker(workDirectory: workDirectory, filename: filename)
+        docker.delegate = self
+        
+        // Start docker
+        dockerQueue.maxConcurrentOperationCount = 1
+        dockerQueue.qualityOfService = .userInitiated
+        dockerQueue.addOperation(docker)
+    }
+    
+    @IBAction func stop(_ sender: Any?) {
+        
+    }
+}
+
+// Docker delegate
+extension EditorWindowController: DockerProtocol {
+    func docker(_ docker: Docker, didReceiveStdout: String) {
+        
+    }
+    
+    func docker(_ docker: Docker, didReceiveStderr: String) {
+        
+    }
+    
+    func docker(_ docker: Docker, didReceiveStdin: String) {
+        
+    }
+    
+    
 }
