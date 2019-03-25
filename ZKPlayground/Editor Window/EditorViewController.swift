2818f40c2c22c0d0d3a27e373a6bc8e4841d234a
Ronald Mannak <ronaldmannak@me.com>
2019-03-25 14:05:17 -0700

    Parse compiler errors

--- ZKPlayground/Editor Window/EditorViewController.swift
+++ ZKPlayground/Editor Window/EditorViewController.swift
@@ -50,54 +50,25 @@ class EditorViewController: NSViewController {
                 let regex = try? NSRegularExpression(pattern: "(\\d+):(\\d+)[\\n\\r\\s]+(.*)", options: [])
             else { return }
             
-//            let matches = regex.matches(in: lint.output, options: [], range: NSRange(location: 0, length: lint.output.count))
-            let string =
-"""
-Compiling /home/zokrates/playground/root.code
-
-Compilation failed:
-
- /home/zokrates/playground:4:3
-    Expected one of [`def`], got `d`
-
- /home/zokrates/playground:5:12
-    Another error message
-"""
-            let matches = regex.matches(in: string, options: [], range: string.fullRange)
+            let matches = regex.matches(in: lint.output, options: [], range: lint.output.fullRange)
             
             var compilerErrors = [CompilerError]()
             
             for match in matches {
                 
-                guard match.numberOfRanges >= 3 else { continue }
+                guard match.numberOfRanges >= 4 else { continue }
                 
-                guard let linenumberString = string.substring(with: match.range(at: 0)),
-                    let columnString = string.substring(with: match.range(at: 1)),
-                let message = string.substring(with: match.range(at: 2)) else {
+                guard let linenumber = Int(String(lint.output.substring(with: match.range(at: 1)) ?? "")),
+                    let column = Int(String(lint.output.substring(with: match.range(at: 2)) ?? "")),
+                    let message = lint.output.substring(with: match.range(at: 3)) else {
                     continue
                 }
                 
-                print("line number \(linenumberString)")
-                print("column \(columnString)")
-                print("message \(message)")
-                
-//                compilerErrors.append(CompilerError(type: .error, line: linenumber, column: column, message: message))
+                let compilerError = CompilerError(type: .error, line: linenumber, column: column, message: String(message))
+                compilerErrors.append(compilerError)
+                print(compilerError)
             }
             
-            // For now, just fetch first error
-            print("\(matches.count) matches")
-            print("\(matches[0].numberOfRanges) ranges")
-//            let range1 = matches[0]
-//            guard matches.count >= 3 else { return }
-        
-//            print("linenumber: \(matches[0])")
-//            print("character: \(matches[1])")
-//            print("message: \(matches[2])")
-        
-            
-//            print(matches)
-//            print(lint.output)
-            // Parse output...
         }
         lintQueue.addOperation(lint)
     }
